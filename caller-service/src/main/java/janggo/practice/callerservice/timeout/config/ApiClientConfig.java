e955d091d0f7670d5401e136a0e26c513711b63d
Jang-GO <jang8195@naver.com>
2025-10-01 18:08:51 +0900

    fix: 설정파일 분리

---
+++ caller-service/src/main/java/janggo/practice/callerservice/timeout/config/ApiClientConfig.java
@@ -0,0 +1,39 @@
+package janggo.practice.callerservice.timeout.config;
+
+import io.netty.channel.ChannelOption;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@Configuration
+public class ApiClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .clientConnector(
+                        new ReactorClientHttpConnector(
+                                HttpClient.create()
+                                        .responseTimeout(Duration.ofSeconds(3))
+                                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public RestClient restClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+        factory.setReadTimeout(Duration.ofSeconds(3));
+        return RestClient.builder().requestFactory(factory).build();
+    }
+}
--- caller-service/src/main/java/janggo/practice/callerservice/timeout/controller/OrderController.java
+++ caller-service/src/main/java/janggo/practice/callerservice/timeout/controller/OrderController.java
@@ -1,39 +1,38 @@
 package janggo.practice.callerservice.timeout.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
     private final WebClient webClient;
 
     private final Logger log = LoggerFactory.getLogger(OrderController.class);
 
-    public OrderController(RestTemplateBuilder restTemplateBuilder, WebClient.Builder webClientBuilder){
-        this.restTemplate = restTemplateBuilder.build();
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
-    }
-
     @GetMapping("/orders/product-details-rt/{productId}")
-    public ResponseEntity<String> getProductDetailsForOrderRestTemplate(@PathVariable("productId") String productId){
+    public ResponseEntity<String> getProductDetailsForOrderRestTemplate(@PathVariable("productId") String productId) {
         String url = "http://localhost:8080/products/" + productId;
         log.info("[RestTemplate] 상품 정보 서비스에 상세 정보를 요청합니다.");
-        String productInfo = restTemplate.getForObject(url, String.class);
+        String productInfo = restClient.get()
+                .uri(url)
+                .retrieve()
+                .body(String.class);
         log.info("[RestTemplate] 상품 정보를 성공적으로 받아왔습니다.");
         return ResponseEntity.ok("[주문 서비스]" + productInfo);
     }
 
     @GetMapping("/orders/product-details-wc/{productId}")
-    public Mono<ResponseEntity<String>> callWithWebClient(@PathVariable("productId") String productId){
+    public Mono<ResponseEntity<String>> callWithWebClient(@PathVariable("productId") String productId) {
         log.info("[WebClient] 상품 정보 서비스에 상세 정보를 요청합니다.");
         return webClient.get()
                 .uri("/products/{productId}", productId)
