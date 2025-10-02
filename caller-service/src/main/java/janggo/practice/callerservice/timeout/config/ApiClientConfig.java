89719680768bb61bc001f5dc071a3e117a745a6f
Jang-GO <jang8195@naver.com>
2025-10-02 22:33:22 +0900

    chore: 생성자 주입 롬복 제거

--- caller-service/src/main/java/janggo/practice/callerservice/timeout/config/ApiClientConfig.java
+++ caller-service/src/main/java/janggo/practice/callerservice/timeout/config/ApiClientConfig.java
@@ -1,13 +1,11 @@
 package janggo.practice.callerservice.timeout.config;
 
 import io.netty.channel.ChannelOption;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -19,6 +17,7 @@ public class ApiClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder
+                .baseUrl("http://localhost:8080")
                 .clientConnector(
                         new ReactorClientHttpConnector(
                                 HttpClient.create()
--- caller-service/src/main/java/janggo/practice/callerservice/timeout/controller/OrderController.java
+++ caller-service/src/main/java/janggo/practice/callerservice/timeout/controller/OrderController.java
@@ -1,6 +1,5 @@
 package janggo.practice.callerservice.timeout.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,22 +11,26 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequiredArgsConstructor
 public class OrderController {
     private final RestClient restClient;
     private final WebClient webClient;
+
+    public OrderController(RestClient restClient, WebClient webClient) {
+        this.restClient = restClient;
+        this.webClient = webClient;
+    }
 
     private final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     @GetMapping("/orders/product-details-rt/{productId}")
     public ResponseEntity<String> getProductDetailsForOrderRestTemplate(@PathVariable("productId") String productId) {
         String url = "http://localhost:8080/products/" + productId;
-        log.info("[RestTemplate] 상품 정보 서비스에 상세 정보를 요청합니다.");
+        log.info("[RestClient] 상품 정보 서비스에 상세 정보를 요청합니다.");
         String productInfo = restClient.get()
                 .uri(url)
                 .retrieve()
                 .body(String.class);
-        log.info("[RestTemplate] 상품 정보를 성공적으로 받아왔습니다.");
+        log.info("[RestClient] 상품 정보를 성공적으로 받아왔습니다.");
         return ResponseEntity.ok("[주문 서비스]" + productInfo);
     }
 
