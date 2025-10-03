019111ec3315b1138ea2b72e653065098a00d20f
Jang-GO <jang8195@naver.com>
2025-10-03 12:29:19 +0900

    chore: retry를 위한 의존성 추가

--- caller-service/src/main/java/janggo/practice/callerservice/CallerServiceApplication.java
+++ caller-service/src/main/java/janggo/practice/callerservice/CallerServiceApplication.java
@@ -2,8 +2,10 @@ package janggo.practice.callerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class CallerServiceApplication {
 
     public static void main(String[] args) {
